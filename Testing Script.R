8577f3560008d2eb4feb58b156d960eb93ed0f97
Agarg98 <aman1998garg@gmail.com>
2020-05-06 23:04:44 +0530

    Subsetting in Lists added

--- Testing Script.R
+++ Testing Script.R
@@ -7,3 +7,16 @@ func <- function(x){
 }
 
 
+x <- list(1, 2, c("A", "B"))
+x
+
+X <- c("A", "B", "C")
+Y <- c("D", "E", "F")
+Z <- 15:17
+matrix1 <- cbind(X, Y, Z)
+matrix1
+
+list1 <- list(one = list(a = list(foo=c(2, 5, 7)), aa=10:12), b="name")
+list1[[1]][[1]][[1]][[3]]
+list1[[c(1,1,1,3)]]
+list1[[c(1,2,3)]]
