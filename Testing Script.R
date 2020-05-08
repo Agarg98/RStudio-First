d922b8db5918c1aaf385d1d92f0811163c805b7b
Agarg98 <aman1998garg@gmail.com>
2020-05-08 14:33:06 +0530

    Practice continues

--- Testing Script.R
+++ Testing Script.R
@@ -12,11 +12,43 @@ x
 
 X <- c("A", "B", "C")
 Y <- c("D", "E", "F")
-Z <- 15:17
-matrix1 <- cbind(X, Y, Z)
-matrix1
+matrix1 <- cbind(X, Y)
+matrix1[5]
+matrix1[1:2, 1, drop=FALSE]
 
 list1 <- list(one = list(a = list(foo=c(2, 5, 7)), aa=10:12), b="name")
 list1[[1]][[1]][[1]][[3]]
 list1[[c(1,1,1,3)]]
 list1[[c(1,2,3)]]
+
+
+newdf <- data.frame(foo=1:3, bar=10:12)
+newdf[, 2]
+newdf[1, 2]
+newdf[2, 1]
+newdf[2]
+
+
+
+make.power <- function(n) {
+  pow <- function(x){
+    x^n
+  }
+}
+
+x<-9
+
+cube <- make.power(3)
+square <- make.power(2)
+
+cube()
+square()
+
+
+
+class(as.character(Sys.time()))
+as.character(Sys.time())
+names(unclass(as.POSIXlt(Sys.time())))
+
+as.POSIXct(Sys.time())
+unclass(as.POSIXct(Sys.time()))
